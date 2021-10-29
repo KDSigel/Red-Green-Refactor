2647d15a5f255dd43d51b2bc18289a9bf5c4fc52
Karl Sigel <heykarl@gmail.com>
2021-10-29 13:20:36 -0700

    added the return to pow and the ... attributes? values? entries? I forget what they are called. Parameters?

--- pow.js
+++ pow.js
@@ -1,5 +1,7 @@
-const pow = () => {
+// Get the test to pass and commit (use a meaningful commit message).
 
+const pow = (base, exponent) => {
+  return Math.pow(base, exponent);
 };
 
 module.exports = pow;
